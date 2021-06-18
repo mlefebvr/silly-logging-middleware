3dc5f35a1325a65cc84ad099adb6a8371d209a51
Martin Lefebvre <dadexter@gmail.com>
2021-06-18 17:18:52 -0400

    Do not log when NODE_ENV is equal to 'test'

--- index.js
+++ index.js
@@ -5,28 +5,30 @@ const log = (request, response, next) => {
   request.timestamp = moment()
   next()
   response.on('finish', () => {
-    const requestTime = request.timestamp
-    const responseTime = moment()
+    if (process.env.NODE_ENV !== 'test') {
+      const requestTime = request.timestamp
+      const responseTime = moment()
 
-    const { statusCode, statusMessage } = response
-    const duration = responseTime.format('x') - requestTime.format('x')
-    const { body, params, query } = request
+      const { statusCode, statusMessage } = response
+      const duration = responseTime.format('x') - requestTime.format('x')
+      const { body, params, query } = request
 
-    console.log(JSON.stringify({
-      environment: process.env.NODE_ENV,
-      app: process.env.APP,
-      timestamp: responseTime,
-      details: {
-        url: request.originalUrl,
-        ...body,
-        ...params,
-        ...query,
-        password: undefined,
-      },
-      statusCode,
-      statusMessage,
-      duration,
-    }))
+      console.log(JSON.stringify({
+        environment: process.env.NODE_ENV,
+        app: process.env.APP,
+        timestamp: responseTime,
+        details: {
+          url: request.originalUrl,
+          ...body,
+          ...params,
+          ...query,
+          password: undefined,
+        },
+        statusCode,
+        statusMessage,
+        duration,
+      }))
+    }
   })
 }
 
